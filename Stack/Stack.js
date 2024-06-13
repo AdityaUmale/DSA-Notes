33665af0bef5945fcc2e01d9cf149ba49f7d3e43
Aditya <aditya.umale17agiledcoders.com>
2024-06-13 23:10:59 +0530

    stack

---
+++ Stack/Stack.js
@@ -0,0 +1,55 @@
+class Stack {
+    constructor(){
+        this.stack = []
+    }
+
+        push(data){
+            this.stack.push(data)
+        }
+
+        pop(data){
+            this.stack.pop()
+        }
+
+        peek(){
+            return this.stack[this.stack.length - 1]
+        }
+
+        isEmpty(){
+            return this.stack.length === 0
+        }
+
+        size(){
+            return this.stack.length
+        }
+
+        clear(){
+            this.stack = []
+        }
+
+        contain(element){
+            return this.stack.includes(element)
+        }
+
+        reverse(){
+            this.stack.reverse()
+        }
+
+        printStack(){
+            let str = ''
+            for (let i = 0; i < this.stack.length; i++) {
+                str += this.stack[i] + "/n"
+                return str
+            }
+        }
+}
+
+//usage example
+
+let myStack = new Stack()
+myStack.push(2)
+myStack.push(3)
+myStack.push(8)
+myStack.pop()
+
+console.log(myStack.printStack())
