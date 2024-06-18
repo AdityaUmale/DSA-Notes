c5a872d5b00addb120457a84f23e949af575836d
Aditya <aditya.umale17agiledcoders.com>
2024-06-18 09:57:19 +0530

    DLL complete

--- linkedList/doublyLinkedList.js
+++ linkedList/doublyLinkedList.js
@@ -26,3 +26,91 @@ DoublyLinkedList.prototype.insertAtBegining = function(data){
         this.tail = newNode
     }
 }
+
+DoublyLinkedList.prototype.insertAtEnd = function(data){
+    const newNode = new Node(data, null, this.tail)
+
+    if (this.tail !== null) {
+        this.tail.next = newNode
+    }
+
+    this.tail = newNode
+
+    if (this.head === null) {
+        this.head = newNode
+    }
+}
+
+// insert at a given node
+
+DoublyLinkedList.prototype.insertAfter = function(prevNode, data){
+    if(prevNode == null) {
+        console.log("invalid prev node")
+        return
+    }
+
+    const newNode = new Node(data,prevNode.next, prevNode)
+
+    if (prevNode.next !== null) {
+        prevNode.next.prev = newNode
+    }
+    prevNode.next = newNode
+
+    if(newNode.next == null) {
+        this.tail = newNode
+    }
+}
+
+// delete first node 
+
+DoublyLinkedList.prototype.deleteFirstNode = function(){
+    if (!this.head) {
+        return
+    }
+
+    if (this.head === this.tail) {
+        this.head = null
+        this.tail = null
+    } else {
+        this.head = this.head.next
+        this.head.prev = null
+    }
+}
+
+// delete last node 
+
+DoublyLinkedList.prototype.deleteLastNode = function() {
+    if (!this.tail) {
+        console.log("DLL is empty")
+        return
+    }
+
+    if (this.head === this.tail) {
+        this.head = null
+        this.tail = null
+    } else {
+        this.tail = this.tail.prev
+        this.tail.next = null
+    }
+}
+
+// reverse DLL 
+
+DoublyLinkedList.prototype.reverse = function() {
+    let current = this.head;
+    let temp = null
+
+    while(current != null) {
+        // swapping
+        temp = current.prev
+        current.prev = current.next
+        current.next = temp;
+        // move to next node 
+        current = current.prev
+    }
+    if (temp != null) {
+        this.tail = this.head
+        this.head = temp.prev
+    }
+
+}
