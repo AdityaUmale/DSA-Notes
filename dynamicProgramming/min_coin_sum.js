a7587a28259a08c9c4ec91a4314f293df5bedad8
Aditya <aditya.umale17agiledcoders.com>
2024-07-18 00:30:10 +0530

    min coin prob

---
+++ dynamicProgramming/min_coin_sum.js
@@ -0,0 +1,18 @@
+function minCoin(coins, amount, memo = {}){
+    if(amount in memo) return memo[amount]
+    if(amount === 0) return 0
+    if(amounr < 0) return -1
+
+    let minCoinsCount = Infinity
+
+    for (const coin of coins) {
+        const result = minCoin(coins, amount - coin, memo)
+        if (result !== -1){
+            minCoinsCount = Math.min(minCoinsCount, result + 1)
+        }
+    }
+
+    memo[amount] = minCoinsCount === Infinity ? -1 : minCoinsCount
+
+    return memo[amount]
+}
